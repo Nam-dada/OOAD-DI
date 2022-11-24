270a63b709cb2c7a84a185510b7a394baa6ece07
YN <1109225291@qq.com>
2022-11-24 20:18:50 +0800

    Finish loading

--- src/dependency_injection/BeanFactoryImpl.java
+++ src/dependency_injection/BeanFactoryImpl.java
@@ -1,18 +1,34 @@
 package dependency_injection;
 
-import java.io.File;
+import java.io.*;
+import java.util.Properties;
 
 /**
  * TODO you should complete the class
  */
 public class BeanFactoryImpl implements BeanFactory {
 
+    public Properties injectPro = new Properties();
+    public Properties valuePro = new Properties();
     @Override
     public void loadInjectProperties(File file) {
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(file));
+            injectPro.load(bf);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public void loadValueProperties(File file) {
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(file));
+            valuePro.load(bf);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
